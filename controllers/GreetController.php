363aa6f9f7f19cbd0edb7e7b86bb0fa92e2c1159
Mykola KOBAK <mykob@smile.fr>
2018-09-23 18:38:00 +0300

    minor changes

--- controllers/GreetController.php
+++ controllers/GreetController.php
@@ -27,8 +27,8 @@ class GreetController extends Controller
     private function checkDate()
     {
         $target_date = new \DateTime('25.09.2018', new \DateTimeZone('Europe/Kiev'));
-        //$now = new \DateTime('now', new \DateTimeZone('Europe/Kiev'));
-        $now = new \DateTime('25.09.2018 11:59:59', new \DateTimeZone('Europe/Kiev'));
+        $now = new \DateTime('now', new \DateTimeZone('Europe/Kiev'));
+        //$now = new \DateTime('25.09.2018 11:59:59', new \DateTimeZone('Europe/Kiev'));
 
         if ($now < $target_date) {
             $this->view->vars('target_date', $target_date);
@@ -39,7 +39,7 @@ class GreetController extends Controller
             if ((int)$now->format('m') === (int)$target_date->format('m') && (int)$now->format('d') === (int)$target_date->format('d')) {
                 return true;
             } else {
-                echo "You are too late";
+                echo "You are too late. Magic is over";
                 return false;
             }
         }
